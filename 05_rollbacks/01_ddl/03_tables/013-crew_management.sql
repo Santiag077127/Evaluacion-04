c0fd6f6486518261589038d340780c9c9a432a5a
Santiag077127 <santiagogordoperez77@gmail.com>
2026-04-20 18:46:55 -0500

    dominio crew management

---
+++ 05_rollbacks/01_ddl/03_tables/013-crew_management.sql
@@ -0,0 +1,11 @@
+-- ============================================
+-- ROLLBACK CREW MANAGEMENT
+-- ============================================
+
+DROP TABLE IF EXISTS crew_schedule;
+
+DROP TABLE IF EXISTS crew_certification;
+
+DROP TABLE IF EXISTS crew_member;
+
+DROP TABLE IF EXISTS crew_role;
